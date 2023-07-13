5379e17f743fb39d9c4c05be5b684bd838430a55
shanon <shanonmambo@gmail.com>
2023-07-13 16:44:31 -0400

    home btn

--- comps/home/Hero.js
+++ comps/home/Hero.js
@@ -60,7 +60,7 @@ export default function Hero() {
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <a
-              href="/becomeaparnter"
+              href="/becomeapartner"
               className="rounded-md bg-green-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-white hover:text-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Get started
